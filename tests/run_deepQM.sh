7aecc0d5ecaa92f5fd99d8985d54f6b26ef27d0c
otayfuroglu <tayfuroglu@gmail.com>
2021-12-17 20:22:27 +0300

    grouped gmx index

--- tests/run_deepQM.sh
+++ tests/run_deepQM.sh
@@ -1,30 +1,46 @@
 #! /usr/bin/env bash
 
-# keyword list:
-#-calcMode: Options -> sp_single_mol, sp_multi_mol, sp_grouped_multi_mol, opt_single_mol, opt_multi_mol, opt_freq_single_mol
-#-struct_dir  # pdb files directory
-#-fbase  for SP calculation
-#-namebase 
-#-seq_start (default=0) pose sarting number
-#-seq_end (default=1E6) pose ending number
-#-group1 M23\ protein keyword in pdb
-#-group2 (default=SOL) lig or sol keyword in pdb
-#-thr_fmax (default=0.01)
 
 deepQM_DIR="/cta/users/otayfuroglu/workspace/deepQM"
-ANI_DIR="/cta/users/akocak/ASE_ANI/"
 PYTHON_DIR="$HOME/miniconda3/bin"
 
-model_list="ani1x ani1ccx ani2x"
-$PYTHON_DIR/python $deepQM_DIR/aniQM.py\
-	-aniDIR $ANI_DIR\
-	-calcMode sp_grouped_multi_mol\
-	-model_list $model_list\
-	-struct_dir PRO\
-	-fbase PRO\
-    	-namebase test_\
-    	-seq_start 0\
-    	-seq_end 8\
-    	-group1 ATOM\
-    	-group2 HETATM\
-    	#-thr_fmax 0.01\
+
+#- set calculation
+# Mode: Options -> sp_single_mol, sp_multi_mol, sp_grouped_multi_mol, opt_single_mol, opt_multi_mol, opt_freq_single_mol
+calcMode=sp_grouped_multi_mol
+
+# define model/s: Available model_list="ani1x ani1ccx ani2x aimnetgas aimnetsmd"
+model_list="ani2x"
+
+
+# struct_dir  # pdb files directory
+struct_dir=2E
+
+
+# fbase  for SP calculation
+fbase=2E
+
+
+# namebase 
+namebase=2ze1_complex
+
+
+# set sequence of start (default=0) pose sarting number
+# set sequence of end (default=1E6) pose ending number
+seq_start=0
+seq_end=1
+
+
+# groups index file
+index_file_path="./2E/2ze1_complex.ndx"
+
+# set group1 M23\ protein keyword in pdb
+# set group2 (default=SOL) lig or sol keyword in pdb
+group1=1
+group2=13
+
+
+# set thrshold fmax for optimization (default=0.01)
+thr_fmax=0.01
+
+$PYTHON_DIR/python $deepQM_DIR/deepQM.py $calcMode $model_list $struct_dir $fbase $namebase $seq_start $seq_end $index_file_path $group1 $group2 $thr_fmax
