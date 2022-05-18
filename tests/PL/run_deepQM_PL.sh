f968466fb3b45dd7763e3c02acc30bfa5e00ce1a
ÖMER TAYFUROĞLU <tayfuroglu@gmail.com>
2022-05-18 12:59:21 +0300

    Update run_deepQM_PL.sh

--- tests/PL/run_deepQM_PL.sh
+++ tests/PL/run_deepQM_PL.sh
@@ -3,53 +3,54 @@
 export CUDA_VISIBLE_DEVICES=0  # one GPU usage
 # export CUDA_VISIBLE_DEVICES=""  # run on CPU
 
-deepQM_DIR="/cta/users/eakkus/deepQM"
-PYTHON_DIR="/cta/users/eakkus/miniconda3/envs/automd/bin"
+# You need to set deepQM and python path for your case
+deepQM_DIR="$HOME/deepQM"
+PYTHON_DIR="$HOME/miniconda3/envs/ANILIE/bin"
 
-
-#- set calculation
+#- set calculation type
 # Mode: Options -> sp_single_mol, sp_multi_mol, sp_grouped_multi_mol,
 # opt_grouped_multi_mol, opt_single_mol, opt_multi_mol
 calcMode=sp_grouped_multi_mol
 
-# set number of processors. It can be GPU or CPU. If using GPU, the GPU-RAM may be limitatition so reduce n_procs for such cases.
+# set number of parallel tasks. It can be GPU or CPU. If using GPU, the GPU-RAM may be limitatition so reduce n_procs for such cases.
 n_procs=2
 
-# define model/s: Available model_list="ani1x ani1ccx ani2x aimnetgas aimnetsmd"
+# define model/s: Available model_list=ani1x, ani1ccx, ani2x, g16, dftd3. One or more models can be set at once.
 model_list="ani2x dftd3"
 
-
-# struct_dir  # includes pdb files dir.
+# The location of pdb files. Explicit directory or subdirectories can be defined.
 struct_dir=$(pwd)/pdb_pro_lig
 
-
-# namebase 
+# This tells what the base name of the pdb files are.
+# When the trajectory is extracted in to multiple files, the file name excluding the frame number is given here. 
+# (e.g. trjmol1.pdb, trjmol2.pdb,... basename=trjmol)
 namebase=trjmol
 
-
-# set sequence of start (default=0) pose sarting number
-# set sequence of end (default=1001) pose ending number
+# frame starting number for the basename (default=0).
 seq_start=0
+
+# frame ending number (default=1000). if -1 is given, it calculates all the pdb files in the struct_dir without caring namebase.
 seq_end=1001
 
-
-# groups index file
-index_file_path=$struct_dir/index.ndx
-# index_file_path="None"
-
-# set group1  (default=Protein) keyword in index
-# set group2 (default=SOL) lig or sol keyword in index
+# Group index file in Gromacs format. 
+# Any two groups can be chosen from index file. 
+# Required when "group" types of calculators are set. 
+# Groups are defined in the following lines
+# IMPORTANT: Full path must be given.
+index_file_path=$(pwd)/index.ndx
 group1="1"
 group2="13"
 
 
-# set thrshold fmax for optimization (default=0.01)
-thr_fmax=0.05
+# sets threshold fmax for optimization (default=0.01). Only used in optimization types of calculators
+thr_fmax=0.01
 
-#maximum iteration for optimization
-maxiter=500
+# Maximum iteration for optimization. Only used in optimization types of calculators
+maxiter=5000
 
 $PYTHON_DIR/python -W ignore $deepQM_DIR/deepQM.py $calcMode $n_procs $model_list $struct_dir $namebase $seq_start $seq_end $index_file_path $group1 $group2 $thr_fmax $maxiter
+
+# For ANI_LIE: alpha=0.000, beta=0.127 and gamma=-5.111. For ANID3_LIE: alpha=-0.0353, beta=0.1487 and gamma=-5.9866. No solvent effects.
 $PYTHON_DIR/python $deepQM_DIR/scripts/bindEnAniD3.py -in "$struct_dir"/"$namebase"_SP_energies_"$group1"_"$group2".csv -ani ani2x -a 0.0 -b 0.127 -g -5.111
 
 
